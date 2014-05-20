91ab3b0c364b899c1ea41a216a6dc1d796018526
samercier <merciers.merciers@gmail.com>
2014-05-20 09:24:49 -0400

    Things

---
+++ kittehvpenguin.rb
@@ -0,0 +1,40 @@
+require_relative 'player'
+require 'gosu'
+
+class GameWindow < Gosu::Window
+	def initialize
+		super 640, 480, false
+		self.caption = "Teh Kittehs"
+
+		@background_image = Gosu::Image.new(self, "media/bg.png", true)
+
+		@player = Player.new(self)
+		@player.warp(320, 240)
+	end
+
+	def update
+		if button_down? Gosu::KbLeft or button_down? Gosu::GpLeft then
+			@player.turn_left
+		end
+		if button_down? Gosu::KbRight or button_down? Gosu::GpRight then
+			@player.turn_right
+		end
+		if button_down? Gosu::KbUp or button_down? Gosu::GpButton0 then
+			@player.accelerate
+		end
+		@player.move
+	end
+
+	def draw
+		@player.draw
+		@background_image.draw(0, 0, 0)
+	end
+	def button_down(id)
+		if id == Gosu::KbEscape
+			close
+		end
+	end
+end
+
+window = GameWindow.new
+window.show
