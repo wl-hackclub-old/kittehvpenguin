34bb2d8f7bcf916b6fd85d106cb920ecae2ac807
samercier <merciers.merciers@gmail.com>
2014-05-20 09:42:57 -0400

    Hey look limits!!!!

--- kittehvpenguin.rb
+++ kittehvpenguin.rb
@@ -14,14 +14,14 @@ class GameWindow < Gosu::Window
 
 	def update
 		if button_down? Gosu::KbLeft or button_down? Gosu::GpLeft then
-			@player.turn_left
+			@player.move_left
 		end
 		if button_down? Gosu::KbRight or button_down? Gosu::GpRight then
-			@player.turn_right
+			@player.move_right
 		end
-		if button_down? Gosu::KbUp or button_down? Gosu::GpButton0 then
-			@player.accelerate
-		end
+		# if button_down? Gosu::KbUp or button_down? Gosu::GpButton0 then
+		# 	@player.accelerate
+		# end
 		@player.move
 	end
 
