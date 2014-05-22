7a94988b8b3c444f808c31cadbc436a68bf6b31e
samercier <merciers.merciers@gmail.com>
2014-05-21 21:54:40 -0400

    Hey look there are menus now!!!

--- kittehvpenguin.rb
+++ kittehvpenguin.rb
@@ -3,7 +3,7 @@ require_relative "player"
 require "gosu"
 
 module ZOrder
-  Background, Stars, Player, UI = *0..3
+	Background, Stars, Player, UI = *0..3
 end
 
 class GameWindow < Gosu::Window
@@ -11,35 +11,55 @@ class GameWindow < Gosu::Window
 		super 640, 480, false
 		self.caption = "Teh Kittehs"
 
-		@font = Gosu::Font.new(self, Gosu::default_font_name, 20)
+		@menu = true
 
 		@background_image = Gosu::Image.new(self, "media/bg.png", true)
 
 		@player = Player.new(self)
 		@player.warp(320, 240)
+
+		@font = Gosu::Font.new(self, Gosu::default_font_name, 20)
 	end
 
 	def update
-		if button_down? Gosu::KbLeft or button_down? Gosu::GpLeft then
-			@player.move_left
+		if !@menu
+			if button_down? Gosu::KbLeft or button_down? Gosu::GpLeft then
+				@player.move_left
+			end
+			if button_down? Gosu::KbRight or button_down? Gosu::GpRight then
+				@player.move_right
+			end
+			# if button_down? Gosu::KbUp or button_down? Gosu::GpButton0 then
+			# 	@player.accelerate
+			# end
+			@player.move
+		else
+			#Menu Controls
+			if button_down? Gosu::KbEnter or button_down? Gosu::KbReturn then
+				@menu = false
+			elsif button_down? Gosu::KbEscape then
+				close
+			end
 		end
-		if button_down? Gosu::KbRight or button_down? Gosu::GpRight then
-			@player.move_right
-		end
-		# if button_down? Gosu::KbUp or button_down? Gosu::GpButton0 then
-		# 	@player.accelerate
-		# end
-		@player.move
+
 	end
 
 	def draw
-		@background_image.draw(0, 0, ZOrder::Background)
-		@player.draw
-		@font.draw("Score: ", 10, 10, ZOrder::UI, 1.0, 1.0, 0xffffff00)
+		if !@menu
+			#Drawing Actors
+			@background_image.draw(0, 0, ZOrder::Background)
+			@player.draw
+			@font.draw("Score: ", 10, 10, ZOrder::UI, 1.0, 1.0, 0xffffff00)
+		else
+			#Drawing Menu
+			@font.draw_rel("Start (enter)", (width / 2) , (height / 2) - 15, ZOrder::UI, 0.5, 0.5, 1.0, 1.0, 0xfff2ff00)
+			@font.draw_rel("Exit (esc)", (width / 2) , (height / 2) + 15, ZOrder::UI, 0.5, 0.5, 1.0, 1.0, 0xfff2ff00)
+		end
 	end
-	def button_down(id)
+	def button_up(id)
+		#Open Menu Code
 		if id == Gosu::KbEscape
-			close
+			@menu = true
 		end
 	end
 end
