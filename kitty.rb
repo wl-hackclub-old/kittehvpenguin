b8768070287dc38acd9c7546ffa209f76883397d
samercier <merciers.merciers@gmail.com>
2014-05-22 22:23:58 -0400

    removed stupid puts

--- kitty.rb
+++ kitty.rb
@@ -11,12 +11,9 @@ class Kitty
 	def move
 		@t += 0.05
 		@t = @t % (Math::PI * 2)
-		# puts @t
 	end
 
 	def draw
-		puts @x
-		puts Math.sin(@t)*120+120
 		@kimg.draw(@x, Math.sin(@t)*120+140, ZOrder::Kitty)
 	end
 end
